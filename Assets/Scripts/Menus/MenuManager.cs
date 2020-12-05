9efee3166e984a859e5a85dc0e9fbfc2553ac48b
Rodrigo Fontes <fontes.rodrigo14@gmail.com>
2020-12-05 14:35:10 -0300

    Version 0.1.4
    Add Graphics options

--- Assets/Scripts/Menus/MenuManager.cs
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -4,15 +4,17 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Rendering.PostProcessing;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject PostFX;
     public Animator menuAnim;
     public Button BotaoJogar, BotaoMultiplayer, BotaoOpcoes, BotaoOpcoes2, CustomizeButton, CustomizeButton2, BotaoSair, BotaoVoltar;
     public GameObject FirstButton, OptionsButton, LocationsButton, skinbutton, ColorPickerButton, HomeButtonSelected;
     [Space(20)]
     public Slider BarraVolume;
-    public Toggle CaixaModoJanela;
+    public Toggle AnisotropicFiltering, VSync, CaixaModoJanela, MotionBlurBox, BloomBox, DepthOfFieldBox;
     public Dropdown Resolucoes, Qualidades;
     public GameObject ColorPickerUI;
     [Space(20)]
@@ -20,22 +22,38 @@ public class MenuManager : MonoBehaviour
     public RectTransform Particle;
     public RectTransform Skins;
     public ControllerManager ControllerManager;
+
+
     int Stars, Fragments;
-    private string nomeDaCena;
-    private float VOLUME;
-    private int qualidadeGrafica, modoJanelaAtivo, resolucaoSalveIndex;
-    private bool telaCheiaAtivada;
-    private Resolution[] resolucoesSuportadas;
+    string nomeDaCena;
+    float VOLUME;
+    int qualidadeGrafica, modoJanelaAtivo, resolucaoSalveIndex, VSyncEnable, AnisotropicFilteringEnable, MotionBlurEnable, BloomEnable, DepthOfFieldEnable;
+    bool telaCheiaAtivada;
+    Resolution[] resolucoesSuportadas;
+
+    DepthOfField DepthOfField = null;
+    MotionBlur MotionBlur = null;
+    Bloom bloomLayer = null;
+    AmbientOcclusion ambientOcclusionLayer = null;
+    ColorGrading colorGradingLayer = null;
     bool OnController = true;
 
     void Awake()
     {
         resolucoesSuportadas = Screen.resolutions;
         ColorPickerUI.GetComponent<ColorPickerUnityUI>().enabled = true;
+        PostFX = GameObject.Find("PostFX");
+        DontDestroyOnLoad(PostFX);
+        PostFX.GetComponent<PostProcessVolume>().profile.TryGetSettings(out DepthOfField);
+        PostFX.GetComponent<PostProcessVolume>().profile.TryGetSettings(out MotionBlur);
+        PostFX.GetComponent<PostProcessVolume>().profile.TryGetSettings(out bloomLayer);
+        PostFX.GetComponent<PostProcessVolume>().profile.TryGetSettings(out ambientOcclusionLayer);
+        PostFX.GetComponent<PostProcessVolume>().profile.TryGetSettings(out colorGradingLayer);
     }
 
     void Start()
     {
+
         ColorPickerUI.GetComponent<ColorPickerUnityUI>().enabled = false;
         FirstButton.GetComponent<Animator>().updateMode = AnimatorUpdateMode.Normal;
         txtStars.text = PlayerPrefs.GetInt("Stars").ToString();
@@ -73,6 +91,11 @@ public class MenuManager : MonoBehaviour
             PlayerPrefs.SetFloat("VOLUME", 1);
             BarraVolume.value = 1;
         }
+
+
+        #region Set Start Box
+
+
         //=============MODO JANELA===========//
         if (PlayerPrefs.HasKey("modoJanela"))
         {
@@ -95,6 +118,8 @@ public class MenuManager : MonoBehaviour
             CaixaModoJanela.isOn = false;
             Screen.fullScreen = true;
         }
+
+
         //========RESOLUCOES========//
         if (modoJanelaAtivo == 1)
         {
@@ -104,6 +129,129 @@ public class MenuManager : MonoBehaviour
         {
             telaCheiaAtivada = true;
         }
+
+
+        //VSync enable/disable
+        if (PlayerPrefs.HasKey("VSync"))
+        {
+            VSyncEnable = PlayerPrefs.GetInt("VSync");
+            if(VSyncEnable == 1)
+            {
+                QualitySettings.vSyncCount = 1;
+                VSync.isOn = true;
+            }
+            else
+            {
+                QualitySettings.vSyncCount = 0;
+                VSync.isOn = false;
+            }
+        }
+        else
+        {
+            VSyncEnable = 1;
+            PlayerPrefs.SetInt("VSync", VSyncEnable);
+            VSync.isOn = true;
+            QualitySettings.vSyncCount = 1;
+        }
+
+
+
+        if (PlayerPrefs.HasKey("Bloom"))
+        {
+            BloomEnable = PlayerPrefs.GetInt("Bloom");
+            if (BloomEnable == 1)
+            {
+                bloomLayer.enabled.value = true;
+                BloomBox.isOn = true;
+            }
+            else
+            {
+                bloomLayer.enabled.value = false;
+                BloomBox.isOn = false;
+            }
+        }
+        else
+        {
+            BloomEnable = 1;
+            PlayerPrefs.SetInt("Bloom", BloomEnable);
+            BloomBox.isOn = true;
+            bloomLayer.enabled.value = true;
+        }
+
+
+
+        if (PlayerPrefs.HasKey("DepthOfField"))
+        {
+            DepthOfFieldEnable = PlayerPrefs.GetInt("DepthOfField");
+            if (DepthOfFieldEnable == 1)
+            {
+                DepthOfField.enabled.value = true;
+                DepthOfFieldBox.isOn = true;
+            }
+            else
+            {
+                DepthOfField.enabled.value = false;
+                DepthOfFieldBox.isOn = false;
+            }
+        }
+        else
+        {
+            DepthOfFieldEnable = 1;
+            PlayerPrefs.SetInt("DepthOfField", DepthOfFieldEnable);
+            DepthOfFieldBox.isOn = true;
+            DepthOfField.enabled.value = true;
+        }
+
+
+
+        if (PlayerPrefs.HasKey("MotionBlur"))
+        {
+            MotionBlurEnable = PlayerPrefs.GetInt("MotionBlur");
+            if (MotionBlurEnable == 1)
+            {
+                MotionBlur.enabled.value = true;
+                MotionBlurBox.isOn = true;
+            }
+            else
+            {
+                MotionBlur.enabled.value = false;
+                MotionBlurBox.isOn = false;
+            }
+        }
+        else
+        {
+            MotionBlurEnable = 1;
+            PlayerPrefs.SetInt("MotionBlur", MotionBlurEnable);
+            MotionBlurBox.isOn = true;
+            MotionBlur.enabled.value = true;
+        }
+
+
+
+        if (PlayerPrefs.HasKey("AnisotropicFiltering"))
+        {
+            AnisotropicFilteringEnable = PlayerPrefs.GetInt("AnisotropicFiltering");
+            if(AnisotropicFilteringEnable == 1)
+            {
+                QualitySettings.anisotropicFiltering = UnityEngine.AnisotropicFiltering.ForceEnable;
+                AnisotropicFiltering.isOn = true;
+            }
+            else
+            {
+                QualitySettings.anisotropicFiltering = UnityEngine.AnisotropicFiltering.Disable;
+                AnisotropicFiltering.isOn = false;
+            }
+        }
+        else
+        {
+            AnisotropicFilteringEnable = 1;
+            PlayerPrefs.SetInt("AnisotropicFiltering", AnisotropicFilteringEnable);
+            AnisotropicFiltering.isOn = true;
+            QualitySettings.anisotropicFiltering = UnityEngine.AnisotropicFiltering.ForceEnable;
+        }
+
+
+
         if (PlayerPrefs.HasKey("RESOLUCAO"))
         {
             resolucaoSalveIndex = PlayerPrefs.GetInt("RESOLUCAO");
@@ -117,6 +265,9 @@ public class MenuManager : MonoBehaviour
             PlayerPrefs.SetInt("RESOLUCAO", resolucaoSalveIndex);
             Resolucoes.value = resolucaoSalveIndex;
         }
+
+
+
         //=========QUALIDADES=========//
         if (PlayerPrefs.HasKey("qualidadeGrafica"))
         {
@@ -132,6 +283,8 @@ public class MenuManager : MonoBehaviour
             Qualidades.value = qualidadeGrafica;
         }
 
+
+        #endregion
         // =========SETAR BOTOES==========//
         BotaoJogar.onClick = new Button.ButtonClickedEvent();
         BotaoOpcoes.onClick = new Button.ButtonClickedEvent();
@@ -189,6 +342,9 @@ public class MenuManager : MonoBehaviour
     //=========VOIDS DE SALVAMENTO==========//
     private void SalvarPreferencias()
     {
+        #region SetPostFX
+
+
         if (CaixaModoJanela.isOn == true)
         {
             modoJanelaAtivo = 1;
@@ -200,8 +356,79 @@ public class MenuManager : MonoBehaviour
             telaCheiaAtivada = true;
         }
 
+
+
+        if(VSync.isOn == true)
+        {
+            VSyncEnable = 1;
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            VSyncEnable = 0;
+            QualitySettings.vSyncCount = 0;
+        }
+
+
+        if (DepthOfFieldBox.isOn == true)
+        {
+            DepthOfFieldEnable = 1;
+            DepthOfField.enabled.value = true;
+        }
+        else
+        {
+            DepthOfFieldEnable = 0;
+            DepthOfField.enabled.value = false;
+        }
+
+
+        if (MotionBlurBox.isOn == true)
+        {
+            MotionBlurEnable = 1;
+            MotionBlur.enabled.value = true;
+        }
+        else
+        {
+            MotionBlurEnable = 0;
+            MotionBlur.enabled.value = false;
+        }
+
+
+
+        if (BloomBox.isOn == true)
+        {
+            BloomEnable = 1;
+            bloomLayer.enabled.value = true;
+        }
+        else
+        {
+            BloomEnable = 0;
+            bloomLayer.enabled.value = false;
+        }
+
+
+
+        if (AnisotropicFiltering.isOn == true)
+        {
+            AnisotropicFilteringEnable = 1;
+            QualitySettings.anisotropicFiltering = UnityEngine.AnisotropicFiltering.ForceEnable;
+        }
+        else
+        {
+            AnisotropicFilteringEnable = 0;
+            QualitySettings.anisotropicFiltering = UnityEngine.AnisotropicFiltering.Disable;
+        }
+
+
+        #endregion
+
         PlayerPrefs.SetFloat("VOLUME", BarraVolume.value);
         PlayerPrefs.SetInt("qualidadeGrafica", Qualidades.value);
+        PlayerPrefs.SetInt("VSync", VSyncEnable);
+        PlayerPrefs.SetInt("AnisotropicFiltering", AnisotropicFilteringEnable);
+        PlayerPrefs.SetInt("Bloom", BloomEnable);
+        PlayerPrefs.SetInt("DepthOfField", DepthOfFieldEnable);
+        PlayerPrefs.SetInt("MotionBlur", MotionBlurEnable);
         PlayerPrefs.SetInt("modoJanela", modoJanelaAtivo);
         PlayerPrefs.SetInt("RESOLUCAO", Resolucoes.value);
         resolucaoSalveIndex = Resolucoes.value;
@@ -278,6 +505,11 @@ public class MenuManager : MonoBehaviour
         txtStars.text = PlayerPrefs.GetInt("Stars").ToString();
         txtFragments.text = PlayerPrefs.GetInt("Fragments").ToString();
         SalvarPreferencias();
+    }
+    
+    public void ClearAllData()
+    {
+        PlayerPrefs.DeleteAll();
     }
 
     private void Jogar()
